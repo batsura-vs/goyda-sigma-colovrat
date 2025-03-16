bd4bb187c33c4c6ab203b4c20831cf2a453b79d0
Vladimir <batsura.vs@gmail.com>
2025-03-16 11:08:18 +0300

    button one

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
 
 import sys
 
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.link = "Not set"
+
+        self.setWindowTitle("Mars Map")
+
+        self.label = QLabel()
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.set_text)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        # Устанавливаем центральный виджет Window.
+        self.setCentralWidget(container)
+
+    def set_text(self, data):
+        self.link = data
+        self.label.setText(f"Current link: {data}")
+
+
 app = QApplication(sys.argv)
 
-
-window = QWidget()
+window = MainWindow()
 window.show()
-
 
 app.exec()
